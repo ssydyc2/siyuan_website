b460eeed2f5cb875f958861a9b92bfa38c512a8a
Siyuan Song <ssydyc2@gmail.com>
2026-07-20 10:39:38 -0700

    feat: refresh about hero traveler

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/siyuan_website/',
+  server: {
+    watch: {
+      ignored: ['**/formal/**/.lake/**'],
+    },
+  },
 })
